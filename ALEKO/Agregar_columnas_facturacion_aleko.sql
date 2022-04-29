162ebeccaf17b0a527a91fad85bb34dab9ba7fbc
Lnavarrete14 <lnavarrete14@gmail.com>
2022-04-29 16:48:22 -0500

    Create Agregar_columnas_facturacion_aleko.sql

---
+++ ALEKO/Agregar_columnas_facturacion_aleko.sql
@@ -0,0 +1,13 @@
+alter table [tbl_aleko_pedido_item_web_app] add id_movimiento_items int 
+
+alter table [tbl_aleko_pedido_item_web_app] add nombre_personalizacion_1 varchar(80) default ''
+alter table [tbl_aleko_pedido_item_web_app] add nombre_personalizacion_2 varchar(80) default ''
+alter table [tbl_aleko_pedido_item_web_app] add nombre_personalizacion_3 varchar(80) default ''
+alter table [tbl_aleko_pedido_item_web_app] add nombre_personalizacion_4 varchar(80) default ''
+alter table [tbl_aleko_pedido_item_web_app] add nombre_personalizacion_5 varchar(80) default ''
+
+alter table [tbl_aleko_pedido_item_web_app] add precio_personalizacion_1 decimal(18,2) default 0.00
+alter table [tbl_aleko_pedido_item_web_app] add precio_personalizacion_2 decimal(18,2) default 0.00
+alter table [tbl_aleko_pedido_item_web_app] add precio_personalizacion_3 decimal(18,2) default 0.00
+alter table [tbl_aleko_pedido_item_web_app] add precio_personalizacion_4 decimal(18,2) default 0.00
+alter table [tbl_aleko_pedido_item_web_app] add precio_personalizacion_5 decimal(18,2) default 0.00
